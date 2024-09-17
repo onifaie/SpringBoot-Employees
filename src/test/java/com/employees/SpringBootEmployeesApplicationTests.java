c61fcaca5e489c57b5cc05a6dc847367b7da3266
onifaie <onifaie@gmail.com>
2024-09-17 23:31:23 +0300

    Frist-Upload-Project

---
+++ src/test/java/com/employees/SpringBootEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
